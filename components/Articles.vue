4b0810da20605e1004586479d28062fd1188eee5
yudaxx <yudax42@gmail.com>
2021-05-15 16:30:43 +0000

    add author to post

--- components/Articles.vue
+++ components/Articles.vue
@@ -11,15 +11,26 @@
       </p>
       <div class="flex">
         <ul class="text-sm text-gray-500 mt-5 dark:text-gray-400 w-9/12">
-          <li class="inline-block mb-3 mr-5">
-            <Icon icon="Write" class="w-5 mb-1 h-5 inline-block mr-2" />Created
-            at: {{ post.created_time }}
-          </li>
           <li class="inline-block">
-            <Icon
-              icon="Refresh"
-              class="w-5 mb-1 h-5 inline-block mr-2"
-            />Updated at: {{ post.created_time }}
+            <Icon icon="Write" class="w-5 mb-1 h-5 inline-block mr-2" />Created
+            at: {{ formateDate(post.last_edited_time) }}
+          </li>
+          <li class="inline-block ml-2" v-if="post.author.length">
+            <Icon icon="Write" class="w-5 mb-1 h-5 inline-block mr-1" />
+            <span>{{ post.author.length > 1 ? 'Authors: ' : 'Author: ' }}</span>
+            <ul class="inline-block ml-1">
+              <li
+                v-for="author in post.author"
+                :key="author.id"
+                class="inline-block mr-3 cursor-pointer"
+              >
+                <img
+                  :src="author.avatar_url"
+                  :key="author.name"
+                  class="inline-block mr-2 w-4 h-4 rounded-full"
+                />{{ author.name }}
+              </li>
+            </ul>
           </li>
         </ul>
         <div class="flex-auto"></div>
@@ -41,6 +52,14 @@ export default {
   },
   components: {
     Icon,
+  },
+  methods: {
+    formateDate(date) {
+      let date_format = new Date(date)
+      return `${date_format.toLocaleString('default', {
+        month: 'short',
+      })} ${date_format.getDay()}, ${date_format.getFullYear()}`
+    },
   },
 }
 </script>
