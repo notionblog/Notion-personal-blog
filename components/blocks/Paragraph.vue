7fff967e532a87361af47c08c0226a4c7c59eae0
yudaxx <yudax42@gmail.com>
2021-05-15 20:01:49 +0000

    add paragraph component with normal annotations

---
+++ components/blocks/Paragraph.vue
@@ -0,0 +1,24 @@
+<template>
+  <p>
+    <!-- {{ p }} -->
+    <span
+      v-for="(item, i) in p"
+      :key="i"
+      :class="{
+        'font-bold': item.annotations.bold,
+        italic: item.annotations.italic,
+        'line-through': item.annotations.strikethrough,
+        underline: item.annotations.underline,
+      }"
+    >
+      {{ item.plain_text }}</span
+    >
+  </p>
+</template>
+<script>
+export default {
+  props: {
+    p: Array,
+  },
+}
+</script>
