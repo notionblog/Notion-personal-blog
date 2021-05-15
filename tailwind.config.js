00d194f1341508dfd30e127ec78e66ee0917b71e
yudaxx <yudax42@gmail.com>
2021-05-15 15:14:00 +0000

    check purge css

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,4 +13,15 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    //any file that may contain the reference of CSS styles by class name.
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js',
+      'nuxt.config.js'
+    ]
+  }
 }
