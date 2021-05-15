718a251314f2633ab808305115ac7206480ae1c7
yudaxx <yudax42@gmail.com>
2021-05-15 06:58:33 +0000

    add footer

---
+++ components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="flex border-t-1 border-gray-400 py-10 text-sm">
+    <span class="inline-block"
+      >&copy; BlogName {{ new Date().getFullYear() }}
+    </span>
+    <div class="flex-auto"></div>
+    <span class="inline-block"
+      >Made with ❤️ by <a href="https://github.com/yudax42">yudax42</a></span
+    >
+  </footer>
+</template>
