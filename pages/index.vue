7564407c08328a9ab930920b44d5b7fc5740fd89
yudaxx <yudax42@gmail.com>
2021-05-16 23:00:34 +0100

    fix post properties date

--- pages/index.vue
+++ pages/index.vue
@@ -32,6 +32,7 @@ export default {
           const postData = {
             id: post.id,
             last_edited_time: post.last_edited_time,
+            created_time: post.created_time,
             title:
               post.properties.Name.title.length > 0 &&
               post.properties.Name.title[0].text
