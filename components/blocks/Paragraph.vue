734b4f47075c3b1f4f9cac35ce0ec306239adb87
yudaxx <yudax42@gmail.com>
2021-05-16 16:46:56 +0100

    add text colors and highlight in paragraph

--- components/blocks/Paragraph.vue
+++ components/blocks/Paragraph.vue
@@ -9,14 +9,32 @@
         'line-through': item.annotations.strikethrough,
         underline: item.annotations.underline,
         'text-red-500': item.annotations.code,
+        'dark:text-red-300': item.annotations.code,
         'p-1.5': item.annotations.code,
         'bg-gray-200': item.annotations.code,
         'dark:bg-gray-900': item.annotations.code,
         'font-light': item.annotations.code,
         'text-base': item.annotations.code,
         'font-serif': item.type == 'equation',
+        'text-gray-100':
+          item.annotations.color != 'default' &&
+          item.annotations.color.split('_')[1],
+        'p-1':
+          item.annotations.color != 'default' &&
+          item.annotations.color.split('_')[1],
       }"
-      class="rounded"
+      :style="{
+        color:
+          item.annotations.color != 'default' &&
+          !item.annotations.color.split('_')[1]
+            ? item.annotations.color
+            : null,
+        'background-color':
+          item.annotations.color != 'default' &&
+          item.annotations.color.split('_')[1]
+            ? item.annotations.color.split('_')[0]
+            : 'none',
+      }"
     >
       <a
         v-if="item.href"
