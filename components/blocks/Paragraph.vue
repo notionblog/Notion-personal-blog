3889527fb2e7e7cf5f3781617cea8aa8bc01162b
yudaxx <yudax42@gmail.com>
2021-05-16 10:29:51 +0100

    add inline-equation and link style

--- components/blocks/Paragraph.vue
+++ components/blocks/Paragraph.vue
@@ -1,6 +1,5 @@
 <template>
   <p>
-    <!-- {{ p }} -->
     <span
       v-for="(item, i) in p"
       :key="i"
@@ -13,9 +12,21 @@
         'p-1': item.annotations.code,
         'bg-gray-800': item.annotations.code,
       }"
+      :style="{
+        'font-family':
+          item.type == 'equation'
+            ? '\'Playfair Display\', Georgia, Times, serif'
+            : 'unset',
+      }"
       class="rounded"
     >
-      {{ item.plain_text }}</span
+      <a
+        v-if="item.href"
+        :href="item.href"
+        target="_blank"
+        class="text-blue-600"
+        >{{ item.plain_text }}</a
+      ><span v-else>{{ item.plain_text }}</span></span
     >
   </p>
 </template>
