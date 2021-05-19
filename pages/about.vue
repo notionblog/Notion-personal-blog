90571e3e117dce9a90c9a043ae67da338a2981bb
yudaxx <yudax42@gmail.com>
2021-05-19 18:12:38 +0100

    change main in  each page

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,5 @@
 <template>
-  <section>
+  <main>
     <h1
       class="text-4xl font-bold capitalize w-full mb-3 text-gray-800 dark:text-gray-100"
     >
@@ -44,7 +44,7 @@
       v-if="postBlocks && postBlocks.length"
       :blocks="postBlocks"
     />
-  </section>
+  </main>
 </template>
 
 <script>
