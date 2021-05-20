a280eac664e2a6c4722bd378a898d016f0365280
yudaxx <yudax42@gmail.com>
2021-05-20 06:58:18 +0100

    fix navbar styling

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -4,7 +4,7 @@
   >
     <nuxt-link to="/"><h1>Yudax</h1></nuxt-link>
     <button
-      class="inline-block outline-none md:hidden border border-gray-300 w-8 h-8 text-gray-400 p-1"
+      class="inline-block outline-none md:hidden border border-gray-300 w-8 h-8 p-1"
       @click="menu = !menu"
     >
       <Icon icon="Menu" />
